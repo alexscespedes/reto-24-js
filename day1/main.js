22063f06f4d76aafedadb2d111e28b9cd89d1cd8
alexscespedes <alexandersenc99@gmail.com>
2023-12-01 11:19:38 -0400

    Day 1 completed

---
+++ day1/main.js
@@ -0,0 +1,8 @@
+function solution(value) {
+    return typeof value;
+}
+
+console.log(solution(1))
+console.log(solution("Dieguillo"))
+console.log(solution(true))
+console.log(solution({name: "alex"}))
